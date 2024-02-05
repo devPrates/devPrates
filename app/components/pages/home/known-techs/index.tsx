95cea1e61bf68e56bb85e73d699de11c5e90cc55
Gabriel Prates Bitencourt <devprates@gmail.com>
2024-02-05 19:51:51 -0400

    Skils Section

---
+++ app/components/pages/home/known-techs/index.tsx
@@ -0,0 +1,23 @@
+import { SectionTitle } from '@/app/components/section-title'
+import { KnownTech } from './known-tech'
+import { TbBrandNextjs } from 'react-icons/tb'
+export const KnownTechs = () => {
+  return (
+    <section className="container py-16">
+      <SectionTitle subtitle="competências" title="Conhecimentos" />
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <KnownTech
+            key={index}
+            tech={{
+              icon: <TbBrandNextjs />,
+              name: 'Nextjs',
+              startDate: '2022-01-01',
+            }}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
